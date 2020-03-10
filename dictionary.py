c8980665bb6fb259a17d4735a2f2affd2026e280
taun323 <andres.lopezg7@gmail.com>
2020-03-10 03:08:29 -0600

    commit

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,2 @@
 d = {"yes": "si", "no": "no", "hi": "hola", "today":"hoy", "good": "bueno","dog":"perro", "cat": "gato", "the":"el", "day":"dia", "is":"es", "a":"un", "sun":"sol", "house":"casa", "car":"carro", "in": "en", "want":"quiere", "enter":"entrar", "good":"bueno", "eat":"comer", "food":"comida"}
+
--- translator.py
+++ translator.py
@@ -13,3 +13,4 @@ except:
 
 print(translate)
 
+
